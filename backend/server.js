6a653736bad9ffae3ba4eb46b06ee2e01a5285ee
Ali Moradi <ali.moradi.design@gmail.com>
2021-06-21 19:00:52 +0430

    fix dirname for deployment

--- backend/server.js
+++ backend/server.js
@@ -64,10 +64,10 @@ app.use('/api/v1/users', users);
 app.use('/api/v1/transactions', transactios);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
 
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
   );
 } else {
   app.get('/', (req, res) => {
